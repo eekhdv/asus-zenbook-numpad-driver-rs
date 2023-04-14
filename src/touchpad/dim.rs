48c0816f45080a3de16ce0bcd965362dbfc18a9f
khadievedem <khadiev.edem@gmail.com>
2023-04-14 10:17:54 +0300

    add touchpad module

---
+++ src/touchpad/dim.rs
@@ -0,0 +1,21 @@
+use evdev_rs::AbsInfo;
+
+
+#[derive(Debug)]
+pub struct TouchpadDimenstions {
+    x: AbsInfo,
+    y: AbsInfo,
+}
+
+impl TouchpadDimenstions {
+    pub fn new(x: AbsInfo, y: AbsInfo) -> Self {
+        Self { x: (x), y: (y) }
+    }
+    pub fn get_max_x(&self) -> i32 {
+        self.x.maximum
+    }
+    pub fn get_max_y(&self) -> i32 {
+        self.y.maximum
+    }
+}
+
